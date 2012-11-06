02981a4b4faa97a740b4dbe8db873df9c6067cd5
Rasmus Nielsen <rasmus.nielsen@me.com>
2012-11-06 16:58:08 +0100

    Goto fixed

---
+++ Expressions/Goto.cs
@@ -0,0 +1,7 @@
+namespace Expressions
+{
+    public class Goto : JumpInstruction
+    {
+        public Goto(string target) : base(Opcode.GOTO, target) { }
+    }
+}
--- Expressions/GotoGoto.cs
+++
@@ -1,7 +0,0 @@
-namespace Expressions
-{
-    public class GotoGoto : JumpInstruction
-    {
-        public GotoGoto(string target) : base(Opcode.GOTO, target) { }
-    }
-}
