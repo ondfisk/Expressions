6366a826b88797ef80f330d1c25e80746380f35c
Rasmus Nielsen <rasmus.nielsen@me.com>
2012-11-06 16:57:15 +0100

    Fixed Generator

--- Expressions/Generator.cs
+++ Expressions/Generator.cs
@@ -8,46 +8,52 @@ namespace Expressions
     /// </summary>
     public class Generator
     {
-        private readonly List<Instruction> instructions;
+        private readonly IList<Instruction> _instructions;
 
         public Generator()
         {
-            instructions = new List<Instruction>();
+            _instructions = new List<Instruction>();
         }
 
         public void Emit(Instruction instr)
         {
-            instructions.Add(instr);
+            _instructions.Add(instr);
         }
 
         public void Label(String label)
         {
-            instructions.Add(new Label(label));
+            _instructions.Add(new Label(label));
         }
 
         public int[] ToBytecode()
         {
             // Pass 1: Build mapping from labels to absolute addresses
-            Dictionary<String, int> labelMap = new Dictionary<string, int>();
-            int address = 0;
-            foreach (Instruction instr in instructions)
+            var labels = new Dictionary<string, int>();
+            var address = 0;
+            foreach (var instr in _instructions)
             {
                 if (instr is Label)
-                    labelMap.Add(((Label)instr).name, address);
+                {
+                    labels.Add((instr as Label).name, address);
+                }
                 else
+                {
                     address += instr.Size;
+                }
             }
             // Pass 2: Use mapping to convert symbolic code to bytes
-            List<int> bytecode = new List<int>();
-            foreach (Instruction instr in instructions)
-                instr.ToBytecode(labelMap, bytecode);
+            var bytecode = new List<int>();
+            foreach (var instruction in _instructions)
+            {
+                instruction.ToBytecode(labels, bytecode);
+            }
             return bytecode.ToArray();
         }
 
         public void PrintCode()
         {
-            int address = 0;
-            foreach (Instruction instr in instructions)
+            var address = 0;
+            foreach (var instr in _instructions)
             {
                 Console.WriteLine("{0,5} {1}", address, instr);
                 address += instr.Size;
